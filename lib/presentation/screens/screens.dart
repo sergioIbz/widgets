408eed70c7c150c68a9e6974fa4c75815f0c45a1
yeison378 <yeisonalexis1@outlook.es>
2023-06-26 18:27:07 -0500

    componentes faltantes

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,4 +1,11 @@
+
+
+export 'package:widgets/presentation/screens/animated/animated_screen.dart';
+export 'package:widgets/presentation/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:widgets/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgets/presentation/screens/cards/cards_screen.dart';
 export 'package:widgets/presentation/screens/home/home_screen.dart';
-
+export 'package:widgets/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widgets/presentation/screens/progress/progress_screen.dart';
+export 'package:widgets/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widgets/presentation/screens/ui_controls/ui_controls_screen.dart';
---
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+class UiControlsScreen extends StatelessWidget {
+  static const name = 'ui_Controls_screen';
+  const UiControlsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
